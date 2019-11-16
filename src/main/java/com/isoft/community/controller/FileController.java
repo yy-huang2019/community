aeb7db95236062098c9f39a9bede6298348bed0f
yy-huang2019 <yyhuang2017@163.com>
2019-11-16 21:04:06 +0800

    富文本编辑的图片添加

--- src/main/java/com/isoft/community/controller/FileController.java
+++ src/main/java/com/isoft/community/controller/FileController.java
@@ -1,4 +1,18 @@
 package com.isoft.community.controller;
 
+import com.isoft.community.dto.FileDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class FileController {
+    @RequestMapping("/file/upload")
+    @ResponseBody         //返回json格式数据
+    public FileDTO upload(){
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/images/background.jpg");
+        return fileDTO;
+    }
 }
--- src/main/java/com/isoft/community/dto/FileDTO.java
+++ src/main/java/com/isoft/community/dto/FileDTO.java
@@ -1,4 +1,10 @@
 package com.isoft.community.dto;
 
-public class FileDTO {
+import lombok.Data;
+
+@Data
+public class FileDTO {                       //文件上传
+    private int success;
+    private String url;
+    private String message;
 }
